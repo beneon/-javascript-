c4a5966e1cdd0fefdc6d84188f9db9c79a96892a
beneon <lucidstudio@gmail.com>
2015-12-26 11:05:08 +0800

    加入bootstrap，准备做一个简历的样子出来

--- jsrender/src/main.js
+++ jsrender/src/main.js
@@ -1,3 +1,7 @@
-$.views.settings.allowCode=true;
-var html = $("#myTemplate").render();
-$("#result").html(html);
+var tmpl = $.templates("#myTemplate")
+// var tmpl = $.templates("Name: {{:name}}");
+var resume = {
+  photo:{src:"img/th.jpg",cName:"thumbnail smallPhoto"}
+}
+var html = tmpl.render(resume)
+$("#result").html(html)
